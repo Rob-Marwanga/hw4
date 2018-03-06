ce9b8d740ee835c0fcd8a75ed41002a906034ab1
Rob-Marwanga <35271718+Rob-Marwanga@users.noreply.github.com>
2018-03-06 03:43:46 -0600

    hw4

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 // KIEI-924 Spring 2018 Homework #4
 // React Native Weather
 
-// Functionality-wise, essentially the same as the jQuery weather application built in 
+// Functionality-wise, essentially the same as the jQuery weather application built in
 // Project #2, without the fade effects, but on mobile!
 
 // HINTS
 // - Built-in "icon" function transforms a Dark Sky icon name into a Font Awesome
-//   icon (yup, Font Awesome works on mobile too, thanks to this awesome open-source 
+//   icon (yup, Font Awesome works on mobile too, thanks to this awesome open-source
 //   project - https://github.com/oblador/react-native-vector-icons)
 // - You use the Font Awesome icon by utilizing the Icon component, like this:
 //   <Icon name="rocket" size={100} color="#000" />. The Icon should live inside a Text
@@ -17,10 +17,10 @@
 //   note that the Icon component is inside parentheses
 // - Math.round(50.85) => 50
 // - For the forecast, use the high temperatures only
-// - Make life easier by storing the entirety of the .daily.data array that comes back 
+// - Make life easier by storing the entirety of the .daily.data array that comes back
 //   from Dark Sky in this.state.forecast, instead of trying to store away the individual
 //   bits and pieces of information
-// - There are styles already written for each component. Use them by adding a "style" 
+// - There are styles already written for each component. Use them by adding a "style"
 //   attribute to each component, e.g. <Text style={styles.currentTemperature}>. See the
 //   styles.js file for the definition of each style.
 // - You'll receive another warning message when looping through the days of the forecast,
@@ -48,7 +48,7 @@ export default class App extends React.Component {
       currentTemperature: "", // the current temperature (e.g. 50.85)
       currentSummary: "",     // the current weather summary (e.g. "Clear throughout the day")
       currentIcon: "",        // the Font Awesome string of the current icon (e.g. "sun-o")
-      forecast: []            // an array holding information on the forecast
+      forecast: [], // an array holding information on the forecast
     };
   }
 
@@ -57,7 +57,7 @@ export default class App extends React.Component {
       locationInputText: text
     });
   }
-  
+
   async getWeather() {
     // Event handler for clicking of the "Get weather!" button
     // Calls the geocoding and weather API, get back a location and weather object
@@ -70,30 +70,58 @@ export default class App extends React.Component {
     this.setState({
       locationName: response.location
     });
-  }
 
-  render() {
-    // Three Views inside the parent view
-    // 1. TextInput and Button for city name and to call getWeather()
-    // 2. Current weather conditions (styles provided with currentIcon, locationText,
-    //    currentTemperature, currentSummary)
-    // 3. Forecast (forecastDay, forecastIcon, forecastTemperature)
-    let forecast = []; // this will eventually hold the JSX elements for each day
+    this.setState({
+      currentTemperature: Math.round(response.weather.currently.temperature) + "°"
+    });
 
-    return (
-      <View style={styles.container}>
-        <View>
-          <TextInput style={{width: 150, height: 40, borderColor: 'gray', borderWidth: 1}} onChangeText={(text) => this.textInputChanged(text)} />
-          <Button onPress={() => this.getWeather()} title="Get the weather!" />
-        </View>
-        <View style={styles.currentWeather}>
-          {/* Current weather conditions */}
-          <Text style={styles.locationText}>{this.state.locationName}</Text>
-        </View>
-        <View style={styles.forecast}>
-          {forecast}
-        </View>
-      </View>
-    );
-  }
+    this.setState({
+      currentSummary: response.weather.currently.summary
+    });
+
+    this.setState({
+      currentIcon: response.weather.currently.icon
+    });
+
+   this.setState({
+    forecast: [response.weather.daily.data]
+  })
+
+};
+
+
+render() {
+  // Three Views inside the parent view
+  // 1. TextInput and Button for city name and to call getWeather()
+  // 2. Current weather conditions (styles provided with currentIcon, locationText,
+  //    currentTemperature, currentSummary)
+  // 3. Forecast (forecastDay, forecastIcon, forecastTemperature)
+
+ // this will eventually hold the JSX elements for each day
+const forecast= []
+ for (let i=1; i<6; i++){
+   forecast.push(<forecastRow forecast={this.state.forecast[i]} key={i}></forecastRow>)
 }
+let forecast
+
+  return (
+    <View style={styles.container}>
+    <View>
+    <TextInput style={{width: 150, height: 40, borderColor: 'gray', borderWidth: 1}} onChangeText={(text) => this.textInputChanged(text)} />
+    <Button onPress={() => this.getWeather()} title="Get the weather!" />
+    </View>
+
+    <View style={styles.currentWeather}>
+      {/* Current weather conditions */}
+    <Text style={styles.locationText}>{this.state.locationName}</Text>
+    <Text style={styles.currentIcon}><Icon size={30} name={icon(this.state.currentIcon)} /></Text>
+    <Text style={styles.currentTemperature}>{this.state.currentTemperature.toString()}</Text>
+    <Text style={styles.currentSummary}>{this.state.currentSummary.toString()}</Text>
+    </View>
+    <View style={styles.forecastDay} key={i}>
+    <Text style={styles.forecastTemperature}>{Math.round(this.state.forecast.[i]toString())}</Text>
+    </View>
+    </View>
+  )
+};
+};
